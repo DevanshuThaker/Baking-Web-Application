902d947b40260ac3c472f715b74644787a3ff35c
DevanshuThaker <utaker834@gmail.com>
2026-08-12 20:20:33 +0530

    Remove hardcoded credentials from MailConfig, use externalized properties

--- src/main/java/com/demo_bank_v1/config/MailConfig.java
+++ src/main/java/com/demo_bank_v1/config/MailConfig.java
@@ -1,30 +1,42 @@
 package com.demo_bank_v1.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+@Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private int mailPort;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
     @Bean
-    public static JavaMailSenderImpl getMailConfig(){
+    public JavaMailSenderImpl getMailConfig(){
         JavaMailSenderImpl emailConfig = new JavaMailSenderImpl();
 
-        // Set Properties:
         Properties props = emailConfig.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
-        // Set Mail Credentials for Gmail SMTP:
-        emailConfig.setHost("smtp.gmail.com");
-        emailConfig.setPort(587);
-        emailConfig.setUsername("devanshu.thaker.2002@gmail.com");
-        emailConfig.setPassword("qayivcvucbkfrded"); // Replace with the App Password you generate
+        emailConfig.setHost(mailHost);
+        emailConfig.setPort(mailPort);
+        emailConfig.setUsername(mailUsername);
+        emailConfig.setPassword(mailPassword);
 
         return emailConfig;
     }
-    // End Of Email Config Method.
 }
